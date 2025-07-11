1ada3b674e1285928f809ea606092dfc461d94ce
JUAN PABLO ARAGON <jparagon2021@gmail.com>
2025-07-11 16:27:28 -0500

    Cors

---
+++ src/main/java/com/forehapp/support_service/config/WebConfig.java
@@ -0,0 +1,30 @@
+package com.forehapp.support_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",
+                                "https://forehapp.netlify.app",
+                                "https://devforehapp.netlify.app",
+                                "https://forehapp.com",
+                                "https://foreapptennis.com"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
